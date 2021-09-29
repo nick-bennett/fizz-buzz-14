3af9320f28e77b7f05f7e75328aa8fd77df894ac
Nick Bennett <nick@nickbenn.com>
2021-09-29 17:02:33 -0600

    Completed JUnit tests.

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -1,11 +1,27 @@
 package edu.cnm.deepdive;
 
+/**
+ * Implements a method that computes the FizzBuzz {@link String} value for any non-negative integer.
+ * This implementation ignores the common form of the FizzBuzz challenge (i.e. "Write the numbers
+ * from 1 to 100, but replace all multiples of 3 with the word 'fizz', all multiples of 5 with the
+ * word 'buzz', and all multiples of <strong>both 3 and 5</strong> with the word 'fizzbuzz'.");
+ * instead, this implementation includes a method that produces the single FizzBuzz value for a
+ * single integer. This allows for more direct unit testing.
+ */
 public class FizzBuzz {
 
   static final String FIZZ_BUZZ_VALUE = "FizzBuzz";
   static final String FIZZ_VALUE = "Fizz";
   static final String BUZZ_VALUE = "Buzz";
 
+  /**
+   * Computes and returns the {@link String} FizzBuzz value for a specified non-negative integer.
+   * Return value will be "Fizz", "Buzz", "FizzBuzz", or the string representation of {@code value},
+   * depending on whether it is divisible by 3, 5, 15, or none of the above.
+   *
+   * @param value {@code int} for which the FizzBuzz value will be computed.
+   * @return {@link String} corresponding to {@code value}.
+   */
   public String getFizzBuzz(int value) {
     String str;
     if (value % 3 == 0) {
